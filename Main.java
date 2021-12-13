5dbef2192d4d56adde47e5505e02f39b647bfdeb
우지 <95010744+otzl@users.noreply.github.com>
2021-12-13 17:57:19 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,163 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 1.로그인 2.회원가입 3.회원목록보기 4.회원정보수정 5.회원탈퇴 6.종료
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("====회원관리 시스템====");
+		MemberDAO dao = new MemberDAO();
+		while (true) {
+			System.out.print("1.로그인 2.회원가입 3.회원목록보기 4.회원정보수정 5.회원탈퇴 6.종료 >>> ");
+			int input = sc.nextInt();
+
+			if (input == 1) {
+				System.out.println("====로그인====");
+
+				System.out.print("아이디 입력 : ");
+				String id = sc.next();
+				System.out.print("비밀번호 입력 : ");
+				String pw = sc.next();
+
+				String nick = dao.login(id, pw);
+				
+				if(nick != null) {
+					System.out.println(nick + "님 환영합니다!");
+					
+					if(id.equals("admin")) {
+						System.out.println("1.회원정보수정 2.회원삭제");
+						input = sc.nextInt();
+						if(input == 1) {
+							System.out.println("====관리자 회원정보수정");
+							System.out.println("아이디 입력 : ");
+							String change_id = sc.next();
+							System.out.println("변경할 닉네임 입력");
+							String change_nick = sc.next();
+							
+							int cnt = dao.adminUpdate(change_id, change_nick);
+							
+							if(cnt > 0) {
+								System.out.println("회원정보 수정 완료");
+							}else {
+								System.out.println("회원정보 수정 실패");
+							}
+						
+							
+							
+							
+						}else if(input == 2) {
+							
+						}
+					}
+					
+					
+					
+				}else {
+					System.out.println("로그인 실패...");
+				}
+
+			} else if (input == 2) {
+
+				System.out.println("====회원가입====");
+				System.out.print("아이디 입력 : ");
+				String id = sc.next();
+				System.out.print("비밀번호 입력 : ");
+				String pw = sc.next();
+				System.out.print("닉네임 입력 : ");
+				String nick = sc.next();
+				int cnt = dao.join(id, pw, nick);
+				
+				if(cnt > 0) {
+					System.out.println("회원가입 성공");
+				}else {
+					System.out.println("회원가입 실패");
+				}
+				// JDBC
+				// 0. JDBC를 사용하는 프로젝트에 Driver 파일넣기
+				// 1. Driver 로딩 (Oracle Driver) -> 동적로딩
+				// 내가 사용하는 DBMS의 드라이버 로딩
+
+				
+
+			} else if (input == 3) {
+				// 회원 목록 보기
+				System.out.println("====회원목록보기====");
+				ArrayList<MemberDTO> list = dao.selectAll();
+				
+				for(int i = 0; i < list.size(); i++) {
+					
+					MemberDTO m = list.get(i);
+					
+					System.out.print(m.getId() + " - ");
+					System.out.print(m.getPw() + " - ");
+					System.out.println(m.getNick());
+				}
+				
+				
+			} else if (input == 4) {
+				// 회원 정보 수정
+				// 0. 프로젝트에 Driver 파일 넣기
+				// 1. 드라이버 로딩
+				// 2. Connection 연결
+				// 3. sql문 작성 및 실행
+				// update 테이블명 set 컬럼명 = 바꾸고 싶은값 where 조건
+				// executeQuery -> 테이블에 데이터가 변함 없을 때
+				// ResultSet로 반환
+				// executeUpdate -> 테이블의 내용이 변경될 때
+				// int 타입으로 반환 -> 실행된 sql문의 수
+				// 4. finally
+				// conn 객체, psmt, rs
+				// 닫아주는 순서 : rs -> psmt -> conn
+
+				// id -> pbk 인 회원의 닉네임을
+				// '킹병관' 으로 바꾸어 주세요
+				System.out.println("====회원정보를 수정해주세요====");
+				System.out.println("아이디 입력 : ");
+				String id = sc.next();
+				int cnt = dao.update(id);
+				
+				if(cnt > 0) {
+					System.out.println("회원정보 수정 완료");
+				}else {
+					System.out.println("회원정보 수정 실패");
+				}
+
+		
+
+				
+
+			} else if (input == 5) {
+				System.out.println("====회원탈퇴====");
+				System.out.print("아이디 입력 : ");
+				String id = sc.next();
+				System.out.print("비밀번호 입력 : ");
+				String pw = sc.next();
+				
+				int cnt = dao.delete(id, pw);
+				
+				if (cnt > 0) {
+					System.out.println("회원삭제 완료");
+				}else {
+					System.out.println("회원삭제 실패");
+				}
+				
+				
+			} else if (input == 6) {
+				System.out.println("프로그램을 종료합니다...");
+				break;
+			} else {
+				System.out.println("정확한 숫자를 다시 입력해주세요.");
+			}
+
+		}
+
+	}
+
+}
---
+++ MemberDAO.java
@@ -0,0 +1,226 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MemberDAO {
+
+	// DAO -> DataBase Access Object
+	// 데이터 베이스에 접근하기 위한 객체를 만들 수 있는 클래스
+	private Connection conn;
+	private PreparedStatement psmt;
+	private ResultSet rs;
+
+	// 드라이버 로딩과 커넥션 객체를 가져오는 메소드
+	private void getConnection() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			String db_url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String db_id = "hr";
+			String db_pw = "hr";
+			try {
+				conn = DriverManager.getConnection(db_url, db_id, db_pw);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// DataBase와 연결을 끊어주는 메소드
+	private void close() {
+		try {
+			if (psmt != null) {
+				psmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+				if (rs != null) {
+					rs.close();
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	Scanner sc = new Scanner(System.in);
+
+	// 로그인 기능
+	public String login(String id, String pw) {
+
+		String nick = null;
+		try {
+
+			// 2. Connection 연결
+			// Connection을 연결하기 위해서는
+			// DB에 접속가능한 url, id, pw가 필요하다
+			String db_url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String db_id = "hr";
+			String db_pw = "hr";
+			conn = DriverManager.getConnection(db_url, db_id, db_pw);
+
+			// 3. SQL문 작성 및 실행
+			String sql = "select * from bigmember where id = ? and pw = ?";
+
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, id);
+			psmt.setString(2, pw);
+
+			rs = psmt.executeQuery();
+
+			if (rs.next()) {
+				nick = rs.getString("nick");
+
+			} else {
+				nick = null;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+
+		}
+		return nick;
+
+	}
+
+	// 회원 가입 기능을 옮겨 주세요
+	// 메소드 이름 -> join
+
+	public int join(String id, String pw, String nick) {
+		int cnt = 0;
+		try {
+			getConnection();
+
+			// 3. SQL문 작성 및 실행
+			String sql = "insert into bigmember values(?, ?, ?)";
+
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, id);
+			psmt.setString(2, pw);
+			psmt.setString(3, nick);
+
+			cnt = psmt.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			close();
+
+		}
+		return cnt;
+
+	}
+
+	// 회원 정보 수정 update
+	public int update(String id) {
+		int cnt = 0;
+		try {
+			getConnection();
+
+			// 3. SQL문 작성 및 실행
+			String sql = "update bigmember set nick = ? where id = ? ";
+
+			psmt = conn.prepareStatement(sql);
+			System.out.print("변경될 닉네임을 입력해주세요>>");
+			psmt.setString(2, id);
+			String nick = sc.next();
+			psmt.setString(1, nick);
+
+			cnt = psmt.executeUpdate(); // executeUpdate = sql실행문장의 갯수를 리턴함
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+
+		}
+		return cnt;
+
+	}
+
+	public ArrayList<MemberDTO> selectAll() {
+		ArrayList<MemberDTO> list = new ArrayList<MemberDTO>();
+
+		getConnection();
+
+		String sql = "select * from bigMember";
+		try {
+			psmt = conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String pw = rs.getString("pw");
+				String nick = rs.getString("nick");
+				MemberDTO m = new MemberDTO(id, pw, nick);
+				list.add(m);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return list;
+
+	}
+
+	public int delete(String id, String pw) {
+		int cnt = 0;
+		getConnection();
+
+		try {
+
+			String sql = "delete from bigmember where id = ? and pw = ?";
+
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(2, pw);
+			psmt.setString(1, id);
+
+			cnt = psmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+
+		}
+		return cnt;
+
+	}
+
+	public int adminUpdate(String change_id, String change_nick) {
+		int cnt = 0;
+		
+		getConnection();
+
+		try {
+			String sql = "update bigmember set nick = ? where id = ?";
+
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, change_nick);
+			psmt.setString(2, change_id);
+			
+			cnt = psmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+
+		}
+		return cnt;
+
+	}
+
+}
